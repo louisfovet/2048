b5527780cc33c99ae4ab3b0318911fea6d8d35b3
Louis Fovet <louiscfovet@gmail.com>
2019-02-24 19:25:26 +0100

    Adding generateNumber (2 or 4)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let matrixHtml = document.getElementById("matrix");
 
+//Matrix of the game
 let matrix = [
     [0,0,0,0],
     [0,0,0,0],
@@ -7,17 +8,24 @@ let matrix = [
     [0,0,0,0]
 ];
 
-
+//Rendring our matrix variable
 function renderMatrix() {
     matrixHtml.innerHTML = null;
     for(let i = 0; i < 4; i++) {
         for(let j = 0; j < 4; j++) {
-            matrixHtml.innerHTML += matrix[i][j]
-            if(j != 3) matrixHtml.innerHTML += " | "
+            matrixHtml.innerHTML += matrix[i][j];
+            if(j != 3) matrixHtml.innerHTML += " | ";
         }
-        matrixHtml.innerHTML += "<br/>"
+        matrixHtml.innerHTML += "<br/>";
     }
+};
+
+//Generates a new number (2 or 4)
+function generateNumber() {
+    number = (Math.floor(Math.random()*2)+1)*2;
+    return number;
 };
 
 
 renderMatrix();
+console.log(generateNew());
